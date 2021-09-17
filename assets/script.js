bd071931bb59df6577c1302c7783e9fdce9c4a58
Faisal L <salluthdev@gmail.com>
2021-09-17 10:48:38 +0700

    Penyesuaian Margin Text dengan Field

--- assets/script.js
+++ assets/script.js
@@ -15,3 +15,40 @@ function toggle(){
 		style.color = '#00ac9f';
 	}
 }
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const passwordValue = password.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username, "Phone Number can't be blank.");
+	} else {
+		setSuccessFor(username);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
